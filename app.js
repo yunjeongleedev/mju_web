752a375c4ef5f51926b04e19ec35b40be034fba4
yunjeongleedev <lee_819@naver.com>
2018-12-05 15:12:59 +0900

    connect with mongodb

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
-
+// var sassMiddleware = require('node-sass-middleware');
+var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -13,17 +13,24 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+//=======================================================
+// mongodb connect
+//=======================================================
+mongoose.Promise = global.Promise; // ES6 Native Promise를 mongoose에서 사용한다.
+const connStr = 'mongodb://localhost/gongmo';
+mongoose.connect(connStr, { useMongoClient: true });
+mongoose.connection.on('error', console.error);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
+// app.use(sassMiddleware({
+//   src: path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   indentedSyntax: true, // true = .sass and false = .scss
+//   sourceMap: true
+// }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
