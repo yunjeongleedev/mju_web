ab64b7b06daf9cd2a07d2bcae385bc0905c030f1
yunjeongleedev <lee_819@naver.com>
2018-12-07 13:50:31 +0900

    login signin

--- app.js
+++ app.js
@@ -1,46 +1,90 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
-// var sassMiddleware = require('node-sass-middleware');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');   
+var session = require('express-session');
+var methodOverride = require('method-override');
+var flash = require('connect-flash');
 var mongoose = require('mongoose');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var questions = require('./routes/questions');
 
 var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.locals.pretty = true;
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
+
+// Pug의 local에 moment라이브러리와 querystring 라이브러리를 사용할 수 있도록.
+app.locals.moment = require('moment');
+app.locals.querystring = require('querystring');
+
 // mongodb connect
 mongoose.Promise = global.Promise; // ES6 Native Promise를 mongoose에서 사용한다.
 const connStr = 'mongodb+srv://YunJeong:arsenal1st!@yunjeongcluster0-cyuda.mongodb.net/test?retryWrites=true';
 mongoose.connect(connStr, {useNewUrlParser: true});
 mongoose.connection.on('error', console.error);
 
+// Favicon은 웹사이트의 대표 아이콘입니다. Favicon을 만들어서 /public에 둡시다.
+// https://www.favicon-generator.org/ 여기서 만들어볼 수 있어요.
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// _method를 통해서 method를 변경할 수 있도록 함. PUT이나 DELETE를 사용할 수 있도록.
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
+
+// sass, scss를 사용할 수 있도록
 // app.use(sassMiddleware({
 //   src: path.join(__dirname, 'public'),
 //   dest: path.join(__dirname, 'public'),
-//   indentedSyntax: true, // true = .sass and false = .scss
+//   indentedSyntax: false, // true = .sass and false = .scss
+//   debug: true,
 //   sourceMap: true
 // }));
+
+// session을 사용할 수 있도록.
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'long-long-long-secret-string-1313513tefgwdsvbjkvasd'
+}));
+
+app.use(flash()); // flash message를 사용할 수 있도록
+
+// public 디렉토리에 있는 내용은 static하게 service하도록.
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// pug의 local에 현재 사용자 정보와 flash 메시지를 전달하자.
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.session.user;
+  res.locals.flashMessages = req.flash();
+  next();
+});
+
+// Route
+app.use('/', index);
+app.use('/users', users);
+app.use('/questions', questions);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
